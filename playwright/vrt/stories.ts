e0d91b11a4ea237ad1690035f461f5bf4d268ed3
SUWA Shigeki <shigeki.suwa@ztrehagem.dev>
2025-09-21 15:37:45 +0900

    filter stories for vrt

--- playwright/vrt/stories.ts
+++ playwright/vrt/stories.ts
@@ -1,4 +1,5 @@
 import { readFile } from "node:fs/promises";
+import { execSync } from "node:child_process";
 
 interface StorybookIndex {
   readonly entries: readonly Story[];
@@ -14,14 +15,89 @@ export interface Story {
   readonly tags: readonly string[];
 }
 
+interface PreviewStats {
+  readonly modules: readonly PreviewStatsModule[];
+}
+
+interface PreviewStatsModule {
+  readonly id: string;
+  readonly name: string;
+  readonly reasons: readonly {
+    readonly moduleName: string;
+  }[];
+}
+
+function walk(set: Set<string>, stats: PreviewStats, id: string): void {
+  if (set.has(id)) {
+    return;
+  }
+
+  const module = stats.modules.find((module) => module.id == id);
+
+  if (module == null) {
+    return;
+  }
+
+  set.add(id);
+
+  for (const reason of module.reasons) {
+    walk(set, stats, reason.moduleName);
+  }
+}
+
 async function readJSON<T>(buffer: Promise<Buffer>): Promise<T> {
   return JSON.parse((await buffer).toString());
 }
 
-const storybookIndex = await readJSON<StorybookIndex>(
-  readFile("storybook-static/index.json"),
-);
+export async function getAffectedStories(): Promise<Story[]> {
+  const storybookIndex = await readJSON<StorybookIndex>(
+    readFile("storybook-static/index.json"),
+  );
 
-export const stories: readonly Story[] = Object.values(
-  storybookIndex.entries,
-).filter((story) => story.type == "story");
+  const stories = Object.values(storybookIndex.entries).filter(
+    (story) => story.type == "story",
+  );
+
+  if (process.env.VRT_ALL != null) {
+    return stories;
+  }
+
+  const diff = execSync(`git diff --name-only origin/main...HEAD`)
+    .toString()
+    .trim()
+    .split("\n");
+
+  if (
+    [
+      /^pnpm-lock\.yaml$/,
+      /^\.storybook\/(main|preview).tsx?$/,
+      /^playwright\.config\.ts$/,
+    ].some((pattern) => diff.some((filepath) => pattern.test(filepath)))
+  ) {
+    return stories;
+  }
+
+  const stats = await readJSON<PreviewStats>(
+    readFile("./storybook-static/preview-stats.json"),
+  );
+
+  const set = new Set<string>();
+
+  const srcDiffIds = diff
+    .filter((filepath) => filepath.startsWith("src/"))
+    .map((filepath) => "./" + filepath);
+
+  for (const id of srcDiffIds) {
+    walk(set, stats, id);
+  }
+
+  const affectedStoryFiles = [...set.values()].filter((id) =>
+    /\.stories\.tsx?/.test(id),
+  );
+
+  const affectedStories = stories.filter((story) =>
+    affectedStoryFiles.includes(story.importPath),
+  );
+
+  return affectedStories;
+}
